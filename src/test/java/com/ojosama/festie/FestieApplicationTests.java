ddeb96c93566b41ada9acbe96a30e7c97e6b3352
gsemily <gsemily22@gmail.com>
2026-04-22 15:52:53 +0900

    init

---
+++ src/test/java/com/ojosama/festie/FestieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojosama.festie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
