6f6ed8990cfcbc279baae2694f877e37d28a6219
YoungJae1118 <apricity1118@gmail.com>
2026-04-22 22:32:39 +0900

    feat: BaseUserEntityEntity 생성

---
+++ common/src/main/java/com/ojosama/common/audit/BaseUserEntity.java
@@ -0,0 +1,24 @@
+package com.ojosama.common.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.util.UUID;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseUserEntity extends BaseEntity {
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private UUID createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private UUID updatedBy;
+
+    @Column(name = "deleted_by")
+    private UUID deletedBy;
+}
