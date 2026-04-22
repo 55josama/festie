6b237cdf6d761df7605f03a8e6fbdf672ec2300f
gsemily <gsemily22@gmail.com>
2026-04-22 18:46:51 +0900

    init

---
+++ common/src/test/java/com/ojosama/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojosama.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
