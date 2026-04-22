d84a4c576f751761bec3dec6cf88b5793b400ecf
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2026-04-22 19:44:27 +0900

    rename: eventservice로 변경

--- event-service/src/main/java/com/ojosama/eventservice/EventServiceApplication.java
+++ event-service/src/main/java/com/ojosama/eventservice/EventServiceApplication.java
@@ -1,4 +1,4 @@
-package com.ojosama.event;
+package com.ojosama.eventservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- event-service/src/test/java/com/ojosama/eventservice/EventServiceApplicationTests.java
+++ event-service/src/test/java/com/ojosama/eventservice/EventServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ojosama.event;
+package com.ojosama.eventservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
