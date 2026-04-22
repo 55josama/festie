07ccc53f2fb6aa07b76a5d29f73f4b18f03d133a
YoungJae <apricity1118@Gmail.com>
2026-04-22 18:45:53 +0900

    init: user 모듈 구현

---
+++ user-service/src/test/java/com/firstlogistics/userservice/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstlogistics.userservice.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
