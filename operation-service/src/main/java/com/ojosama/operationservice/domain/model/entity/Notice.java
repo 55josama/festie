284c276340b676f0fa6836572a54f40f5956fa2d
gsemily <gsemily22@gmail.com>
2026-04-24 04:09:39 +0900

    fix: notice entity schema명 수정

--- operation-service/src/main/java/com/ojosama/operationservice/domain/model/entity/Notice.java
+++ operation-service/src/main/java/com/ojosama/operationservice/domain/model/entity/Notice.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "p_notice", schema = "operation-service")
+@Table(name = "p_notice", schema = "operation_service")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notice extends BaseEntity {
     @Id
