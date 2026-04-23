2c0778ccadaa0d359cd676a5168d6843c40bf443
gsemily <gsemily22@gmail.com>
2026-04-24 03:08:00 +0900

    feat: 공지사항 entity 추가

---
+++ operation-service/src/main/java/com/ojosama/operationservice/domain/model/entity/Notice.java
@@ -0,0 +1,53 @@
+package com.ojosama.operationservice.domain.model.entity;
+
+import com.ojosama.common.audit.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "p_notice", schema = "operation-service")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notice extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "admin_id", nullable = false)
+    private UUID adminId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Builder
+    private Notice(UUID adminId, String title, String content) {
+        this.adminId = adminId;
+        this.title = title;
+        this.content = content;
+    }
+
+    public static Notice of(UUID adminId, String title, String content) {
+        return Notice.builder()
+                .adminId(adminId)
+                .title(title)
+                .content(content)
+                .build();
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
