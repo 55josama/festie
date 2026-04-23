e1c04b1eddb97c3dce7be85e0d68f06ff4538c33
KimRanmi <rmkim4@gmail.com>
2026-04-23 18:37:00 +0900

    [Feature] favorite 도메인, 레포지토리, 컨피그 서버 설정 추가 (#24)
    * chore : build.gradle 수정
    
    * chore : yaml파일 수정
    
    * refactor : 패키지 이름 변경
    
    * docs : 나중에 삭제 할 파일
    
    * feat : repository 추가
    
    * feat : entity 추가
    
    * chore : build.gradle h2database 추가
    
    * fix : baseEntity 추가
    
    * remove : event vo 삭제
    
    * refactor : event vo -> eventId, categoryId 수정
    
    * refactor : baseEntity 추가

---
+++ favorite-service/src/main/java/com/ojosama/favoriteservice/domain/model/Favorite.java
@@ -0,0 +1,35 @@
+package com.ojosama.favoriteservice.domain.model;
+
+import com.ojosama.common.audit.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+@Getter
+@Entity
+@Table(name = "p_favorite")
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class Favorite extends BaseEntity {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "event_id")
+    private UUID eventId;
+
+    @Column(name = "category_id")
+    private UUID categoryId;
+
+}
