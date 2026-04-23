b2f8227fde10bd8a2a1dff9fc146d17e0ec1c881
rmkim4@gmail.com <rmkim4@gmail.com>
2026-04-23 17:25:16 +0900

    refactor : 패키지 이름 변경

--- favorite-service/src/test/java/com/ojosama/favoriteservice/FavoriteServiceApplicationTests.java
+++ favorite-service/src/test/java/com/ojosama/favoriteservice/FavoriteServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.festie.favoriteservice;
+package com.ojosama.favoriteservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FavoriteServiceApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+    @Test
+    void contextLoads() {
+    }
 
 }
