8e4724036c387b4e066f3d81ee751f6c8ebff42d
YoungJae1118 <apricity1118@gmail.com>
2026-04-24 10:38:04 +0900

    feat: UserService 유저 생성 메서드 추가 refactor: User 엔티티 Create -> create로 수정

--- user-service/src/main/java/com/ojosama/userservice/application/service/UserService.java
+++ user-service/src/main/java/com/ojosama/userservice/application/service/UserService.java
@@ -1,4 +1,37 @@
 package com.ojosama.userservice.application.service;
 
+import com.ojosama.userservice.application.dto.command.CreateUserCommand;
+import com.ojosama.userservice.application.dto.result.CreateUserResult;
+import com.ojosama.userservice.domain.model.User;
+import com.ojosama.userservice.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    //유저 생성
+    @Transactional
+    public CreateUserResult createUser(CreateUserCommand command) {
+        User user = User.create(
+                command.email(),
+                command.password(),
+                command.nickname(),
+                command.name()
+        );
+
+        User savedUser = userRepository.save(user);
+
+        return new CreateUserResult(
+                savedUser.getId(),
+                savedUser.getEmail(),
+                savedUser.getNickname(),
+                savedUser.getName(),
+                savedUser.getRole()
+        );
+    }
 }
