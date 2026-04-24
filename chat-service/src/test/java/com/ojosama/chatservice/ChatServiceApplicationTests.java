4053c738cda1178b280aafff4d03413e9f964e3d
subin <143770193+LSLight@users.noreply.github.com>
2026-04-24 12:51:24 +0900

    [BUGFIX]  chat-service 빌드시 오류 해결 (#32)
    * fix : repository 구현체 필드명 수정, 메소드명 수정
    
    * feat : build 시 test db(h2) 추가
    
    * test : 테스트 전용 설정 Eureka 해제
    
    * fix : 코드래빗 수정안 반영 h2 db에 스키마 설정

--- chat-service/src/test/java/com/ojosama/chatservice/ChatServiceApplicationTests.java
+++ chat-service/src/test/java/com/ojosama/chatservice/ChatServiceApplicationTests.java
@@ -2,12 +2,14 @@ package com.ojosama.chatservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ChatServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
