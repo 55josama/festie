117bd43ce78ee9531de58de976dff323d8762af5
YoungJae1118 <apricity1118@gmail.com>
2026-04-24 12:02:00 +0900

    feat: 유저 단건 조회 기능 추가

--- user-service/src/main/java/com/ojosama/userservice/presentation/controller/UserController.java
+++ user-service/src/main/java/com/ojosama/userservice/presentation/controller/UserController.java
@@ -1,24 +1,32 @@
 package com.ojosama.userservice.presentation.controller;
 
+import com.ojosama.userservice.application.dto.query.GetUserQuery;
 import com.ojosama.userservice.application.dto.result.CreateUserResult;
+import com.ojosama.userservice.application.dto.result.GetUserResult;
 import com.ojosama.userservice.application.service.UserService;
 import com.ojosama.userservice.presentation.dto.request.CreateUserRequestDto;
 import com.ojosama.userservice.presentation.dto.response.CreateUserResponseDto;
+import com.ojosama.userservice.presentation.dto.response.GetUserResponseDto;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private UserService userService;
 
     //유저 생성
-    @PostMapping("/users")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CreateUserResponseDto createUser(@RequestBody CreateUserRequestDto request) {
         CreateUserResult result = userService.createUser(request.toCommand());
@@ -26,5 +34,16 @@ public class UserController {
         return CreateUserResponseDto.from(result);
     }
 
-    //
+    //유저 단건 조회
+    @GetMapping("/{userId}")
+    public GetUserResponseDto getUser(@PathVariable UUID userId) {
+        GetUserQuery query = new GetUserQuery(userId);
+
+        GetUserResult result = userService.getUser(query);
+
+        return GetUserResponseDto.from(result);
+    }
+
+    //유저 수정
+//    @PatchMapping("/{userId}")
 }
