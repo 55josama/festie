3ee74e468d6b43ab090652759db3b1c31b51b902
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2026-04-24 12:12:46 +0900

    feat: EventCategoryRepository 추가

---
+++ event-service/src/main/java/com/ojosama/eventservice/event/infrastructor/persistence/EventCategoryRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.ojosama.eventservice.event.infrastructor.persistence;
+
+import com.ojosama.eventservice.event.domain.repository.EventCategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class EventCategoryRepositoryImpl implements EventCategoryRepository {
+
+    private final JpaEventCategoryRepository jpaEventCategoryRepository;
+}
---
+++ event-service/src/main/java/com/ojosama/eventservice/event/infrastructor/persistence/JpaEventCategoryRepository.java
@@ -0,0 +1,8 @@
+package com.ojosama.eventservice.event.infrastructor.persistence;
+
+import com.ojosama.eventservice.event.domain.model.EventCategory;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaEventCategoryRepository extends JpaRepository<EventCategory, UUID> {
+}
