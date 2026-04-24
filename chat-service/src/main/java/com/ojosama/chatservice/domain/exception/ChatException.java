72e612540b1833fe6c192bdcf1b634273e96cfbb
Lee Subin <99sbin@gmail.com>
2026-04-24 12:29:48 +0900

    feat : ChatException 파라미터를 ErrorCode 로 변경

--- chat-service/src/main/java/com/ojosama/chatservice/domain/exception/ChatException.java
+++ chat-service/src/main/java/com/ojosama/chatservice/domain/exception/ChatException.java
@@ -1,9 +1,10 @@
 package com.ojosama.chatservice.domain.exception;
 
 import com.ojosama.common.exception.CustomException;
+import com.ojosama.common.exception.ErrorCode;
 
 public class ChatException extends CustomException {
-    public ChatException(ChatErrorCode code) {
+    public ChatException(ErrorCode code) {
         super(code);
     }
 }
