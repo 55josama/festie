4e66a6933cbdabc469398566f6473857b7784430
gsemily <gsemily22@gmail.com>
2026-04-24 21:14:52 +0900

    feat: AiReportEvent payload 작성

---
+++ operation-service/src/main/java/com/ojosama/operationservice/domain/event/payload/AiReportEvent.java
@@ -0,0 +1,16 @@
+package com.ojosama.operationservice.domain.event.payload;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AiReportEvent {
+    private UUID targetId;
+    private String targetType;
+    private String category;
+    private String description;
+}
