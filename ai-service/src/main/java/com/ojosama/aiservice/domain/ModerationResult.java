690216a024778452a5908b505b8dd37deea5f4e3
gsemily <gsemily22@gmail.com>
2026-04-27 19:13:32 +0900

    refactor: 도메인별 패키지 분리

--- ai-service/src/main/java/com/ojosama/aiservice/domain/ModerationResult.java
+++
@@ -1,9 +0,0 @@
-package com.ojosama.aiservice.domain;
-
-import com.ojosama.operationservice.domain.model.enums.ReportCategory;
-
-public record ModerationResult(
-        boolean isViolating,
-        ReportCategory category,
-        String reason
-) { }
