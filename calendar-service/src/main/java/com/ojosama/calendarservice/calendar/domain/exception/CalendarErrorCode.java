f2184df07252cddc6853d88ba75f88012001741c
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2026-04-27 12:43:58 +0900

    feat: Calendar ErrorCode, Exception 추가

---
+++ calendar-service/src/main/java/com/ojosama/calendarservice/calendar/domain/exception/CalendarErrorCode.java
@@ -0,0 +1,27 @@
+package com.ojosama.calendarservice.calendar.domain.exception;
+
+import com.ojosama.common.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum CalendarErrorCode implements ErrorCode {
+
+    CALENDAR_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 일정을 찾을 수 없습니다."),
+    CALENDAR_ACCESS_DENIED(HttpStatus.FORBIDDEN, "다른 사용자의 일정은 수정할 수 없습니다."),
+    CALENDAR_DELETE_DENIED(HttpStatus.FORBIDDEN, "다른 사용자의 일정은 삭제할 수 없습니다."),
+    EVENT_SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 행사 일정을 찾을 수 없습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    @Override
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
