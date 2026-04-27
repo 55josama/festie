e5435c6e5668566b02a120549a3b7022f601089e
gsemily <gsemily22@gmail.com>
2026-04-27 18:45:35 +0900

    fix:x: 실행 범위 재설정

--- operation-service/src/main/java/com/ojosama/OperationServiceApplication.java
+++ operation-service/src/main/java/com/ojosama/OperationServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackages = "com.ojosama.operationservice")
+@EnableFeignClients(basePackages = "com.ojosama")
 @SpringBootApplication
 public class OperationServiceApplication {
 
