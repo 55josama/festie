8ee0b39b725ac5026a8192541201a3e0c9986f7e
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2026-04-28 15:41:14 +0900

    chore: JpaAuditing 추가

--- event-service/src/main/java/com/ojosama/eventservice/EventServiceApplication.java
+++ event-service/src/main/java/com/ojosama/eventservice/EventServiceApplication.java
@@ -3,13 +3,15 @@ package com.ojosama.eventservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableJpaAuditing
 public class EventServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventServiceApplication.class, args);
+    }
 
 }
