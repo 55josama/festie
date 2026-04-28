612483f14d2340a8a703ab5c735d8ebb945d8c21
gsemily <gsemily22@gmail.com>
2026-04-28 12:42:19 +0900

    feafeat: ì±t: 채팅과 커뮤니티의 다른 batch 처리를 위 위한 kafka coonfig 설정

---
+++ ai-service/src/main/java/com/ojosama/moderation/infrastructure/config/KafkaConfig.java
@@ -0,0 +1,45 @@
+package com.ojosama.moderation.infrastructure.config;
+
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+
+@Configuration
+public class KafkaConfig {
+    // 채팅 데이터 처리를 위한 팩토리(빈번하게 일어남)
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> chatBatchFactory(
+            ConsumerFactory<String, Object> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setBatchListener(true);
+
+        // 최대 100개의 데이터를 가져오고, 데이터가 다 안 차더라도 0.1초가 지나면 가져오도록 설정
+        Properties props = new Properties();
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100);
+        props.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, 100);
+        factory.getContainerProperties().setKafkaConsumerProperties(props);
+
+        return factory;
+    }
+
+    // 게시글/댓글 데이터 처리를 위한 팩토리(덜 빈번하게 일어남)
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> communityBatchFactory(
+            ConsumerFactory<String, Object> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setBatchListener(true);
+
+        // 최대 50개의 데이터를 가져오고, 데이터가 다 안 차더라도 2초가 지나면 가져오도록 설정
+        Properties props = new Properties();
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 50);
+        props.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, 2000);
+        factory.getContainerProperties().setKafkaConsumerProperties(props);
+
+        return factory;
+    }
+}
