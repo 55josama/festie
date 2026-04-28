a1ae6d1f5be3540ef8fad8dd650173bd2aef0f42
YoungJae1118 <apricity1118@gmail.com>
2026-04-28 15:43:47 +0900

    feat: email 중복체크

--- user-service/src/main/java/com/ojosama/userservice/domain/repository/UserRepository.java
+++ user-service/src/main/java/com/ojosama/userservice/domain/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.ojosama.userservice.domain.repository;
 
 import com.ojosama.userservice.domain.model.User;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
