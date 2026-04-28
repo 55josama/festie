64f5bf4ddeac9effeae585f669d94a9cd5825d6d
rmkim4@gmail.com <rmkim4@gmail.com>
2026-04-28 11:09:21 +0900

    fix : 도메인 설정 수장

--- notification-service/src/main/java/com/ojosama/notificationservice/domain/model/notification/TargetInfo.java
+++ notification-service/src/main/java/com/ojosama/notificationservice/domain/model/notification/TargetInfo.java
@@ -1,11 +1,14 @@
 package com.ojosama.notificationservice.domain.model.notification;
 
+import com.ojosama.notificationservice.domain.exception.NotificationErrorCode;
+import com.ojosama.notificationservice.domain.exception.NotificationException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import java.util.UUID;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,4 +28,54 @@ public class TargetInfo {
     @Enumerated(EnumType.STRING)
     private TargetType targetType;
 
+    @Builder
+    private TargetInfo(UUID targetId, Target target, TargetType targetType) {
+        validate(targetId, target, targetType);
+        this.targetId = targetId;
+        this.target = target;
+        this.targetType = targetType;
+    }
+
+    public static TargetInfo of(UUID targetId, Target target, TargetType targetType) {
+        return TargetInfo.builder()
+                .targetId(targetId)
+                .target(target)
+                .targetType(targetType)
+                .build();
+    }
+
+    public static TargetInfo operation(UUID targetId) {
+        return TargetInfo.builder()
+                .targetId(targetId)
+                .target(Target.OPERATION)
+                .targetType(TargetType.REPORT_CREATED)
+                .build();
+    }
+
+    // 티켓팅 리마인드
+    public static TargetInfo ticketing(UUID targetId) {
+        return TargetInfo.builder()
+                .targetId(targetId)
+                .target(Target.EVENT)
+                .targetType(TargetType.TICKETING_REMINDER)
+                .build();
+    }
+
+    // 행사 리마인드
+    public static TargetInfo event(UUID targetId) {
+        return TargetInfo.builder()
+                .targetId(targetId)
+                .target(Target.EVENT)
+                .targetType(TargetType.EVENT_REMINDER)
+                .build();
+    }
+
+    private void validate(UUID targetId, Target target, TargetType targetType) {
+        if (targetId == null) {
+            throw new NotificationException(NotificationErrorCode.INVALID_TARGET_ID);
+        }
+        if (target == null || targetType == null) {
+            throw new NotificationException(NotificationErrorCode.INVALID_TARGET_TYPE);
+        }
+    }
 }
