6b256e62de8098ffdc43299f1a079e9cc0bc5fad
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2026-04-28 15:42:07 +0900

    chore: ComponentScan 추가

--- event-service/src/main/java/com/ojosama/eventservice/EventServiceApplication.java
+++ event-service/src/main/java/com/ojosama/eventservice/EventServiceApplication.java
@@ -2,11 +2,13 @@ package com.ojosama.eventservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
+@ConfigurationPropertiesScan
 @EnableJpaAuditing
 public class EventServiceApplication {
 
