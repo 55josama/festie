217cbf14f6931ffa5b94e7fba360095b8eaf1f56
rmkim4@gmail.com <rmkim4@gmail.com>
2026-04-28 22:28:19 +0900

    feat : 에러코드 추가

--- calendar-service/src/main/java/com/ojosama/calendarservice/calendar/domain/exception/CalendarErrorCode.java
+++ calendar-service/src/main/java/com/ojosama/calendarservice/calendar/domain/exception/CalendarErrorCode.java
@@ -11,7 +11,9 @@ public enum CalendarErrorCode implements ErrorCode {
     CALENDAR_ACCESS_DENIED(HttpStatus.FORBIDDEN, "다른 사용자의 일정은 수정할 수 없습니다."),
     CALENDAR_DELETE_DENIED(HttpStatus.FORBIDDEN, "다른 사용자의 일정은 삭제할 수 없습니다."),
     EVENT_SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 행사 일정을 찾을 수 없습니다."),
-    INVALID_INPUT(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다.");
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다."),
+
+    EXISTS_CALENDAR(HttpStatus.NOT_FOUND, "존재하는 일정입니다.");
 
     private final HttpStatus status;
     private final String message;
