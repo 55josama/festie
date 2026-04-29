45cf3e9753a0cef14cbda25b2bba6a0ebec00e55
gsemily <120404242+gsemily@users.noreply.github.com>
2026-04-29 15:16:00 +0900

    [REFACTOR] operation, ai-service에 outbox 패턴 도입 (#83)
    * feat: Scheduling 적용
    
    * feat: Outbound 추가
    
    * feat: 이벤트 송신 outbox 도입
    
    * refactor: 불필요한 필드 삭제
    
    * feat: 이벤트 송신 outbox 도입
    
    * feat: 이벤트 수신 inbox 도입
    
    * feat: @EnableScheduling 추가
    
    * refactor: outbox 패턴 도입 및 수정
    
    * refactor: inbox 패턴 도입 및  수정
    
    * refactor: 원문 페이로드 로깅 방지 및 실패 시 메시지 영구 유실 방지

--- operation-service/src/main/java/com/ojosama/OperationServiceApplication.java
+++ operation-service/src/main/java/com/ojosama/OperationServiceApplication.java
@@ -3,7 +3,9 @@ package com.ojosama;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableFeignClients(basePackages = "com.ojosama")
 @SpringBootApplication
 public class OperationServiceApplication {
