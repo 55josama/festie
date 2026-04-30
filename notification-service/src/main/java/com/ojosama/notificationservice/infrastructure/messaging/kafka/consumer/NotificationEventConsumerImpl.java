3800ffd881765cdb9c5114511aeaca9926da20d6
rmkim4@gmail.com <rmkim4@gmail.com>
2026-04-30 18:32:56 +0900

    remove : 필요없는 파일 삭제

--- notification-service/src/main/java/com/ojosama/notificationservice/infrastructure/messaging/kafka/consumer/NotificationEventConsumerImpl.java
+++
@@ -1,6 +0,0 @@
-package com.ojosama.notificationservice.infrastructure.messaging.kafka.consumer;
-
-import com.ojosama.notificationservice.domain.event.NotificationEventConsumer;
-
-public class NotificationEventConsumerImpl implements NotificationEventConsumer {
-}
