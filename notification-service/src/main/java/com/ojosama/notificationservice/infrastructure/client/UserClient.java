f1cf57e1b0fc33deeaeb683ae5120aed7d40a4eb
rmkim4@gmail.com <rmkim4@gmail.com>
2026-04-30 20:46:01 +0900

    feat: UserClient의 userEmail 조회 방식 변경 (GET → POST) 및 @RequestBody 추가

--- notification-service/src/main/java/com/ojosama/notificationservice/infrastructure/client/UserClient.java
+++ notification-service/src/main/java/com/ojosama/notificationservice/infrastructure/client/UserClient.java
@@ -7,14 +7,16 @@ import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "user-service", fallbackFactory = UserClientFallBackFactory.class, primary = false)
 public interface UserClient {
 
     // userEmail 벌크 조회
-    @GetMapping("/internal/v1/users/emails")
-    UserInfo getUserInfo(List<UUID> userIds);
+    @PostMapping("/internal/v1/users/emails")
+    UserInfo getUserInfo(@RequestBody List<UUID> userIds);
 
     @GetMapping("/internal/v1/users/{userId}/email")
     String getUserEmail(@PathVariable("userId") UUID userId);
