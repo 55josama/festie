e11da6aefba0a9084400180ddf35d5f94b95d6c3
YoungJae1118 <apricity1118@gmail.com>
2026-05-02 04:47:33 +0900

    feat: GatewayServer 모듈 생성

--- user-service/src/main/java/com/ojosama/userservice/infrastructure/messaging/kafka/consumer/UserBlacklistStatusEventConsumer.java
+++ user-service/src/main/java/com/ojosama/userservice/infrastructure/messaging/kafka/consumer/UserBlacklistStatusEventConsumer.java
@@ -11,11 +11,13 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@Profile("!local")
 @RequiredArgsConstructor
 public class UserBlacklistStatusEventConsumer {
 
