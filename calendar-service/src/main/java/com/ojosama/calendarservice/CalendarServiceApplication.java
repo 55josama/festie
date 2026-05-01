ae7950f4ea63101bcf9001516425e8018c371b1c
rmkim4@gmail.com <rmkim4@gmail.com>
2026-05-01 21:50:00 +0900

    feat: 스케줄링 및 JPA 설정 추가현

--- calendar-service/src/main/java/com/ojosama/calendarservice/CalendarServiceApplication.java
+++ calendar-service/src/main/java/com/ojosama/calendarservice/CalendarServiceApplication.java
@@ -2,12 +2,26 @@ package com.ojosama.calendarservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableJpaAuditing
+@EnableScheduling
+@ComponentScan(basePackages = "com.ojosama")
+@EntityScan(basePackages = {
+        "com.ojosama.calendarservice.calendar.domain.model",
+        "com.ojosama.common.kafka.domain"
+})
+@EnableJpaRepositories(basePackages = {
+        "com.ojosama.calendarservice.calendar.infrastructure.persistence",
+        "com.ojosama.common.kafka.domain"
+})
 public class CalendarServiceApplication {
 
     public static void main(String[] args) {
