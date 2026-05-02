aa11b9da708e347cdd8896d60b7766892045cfb8
gsemily <gsemily22@gmail.com>
2026-05-03 00:35:11 +0900

    refactor: or: 파일 병합 및 파일명 수정

---
+++ favorite-service/src/main/generated/com/ojosama/favoriteservice/domain/model/QFavorite.java
@@ -0,0 +1,75 @@
+package com.ojosama.favoriteservice.domain.model;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QFavorite is a Querydsl query type for Favorite
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QFavorite extends EntityPathBase<Favorite> {
+
+    private static final long serialVersionUID = 325442929L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QFavorite favorite = new QFavorite("favorite");
+
+    public final com.ojosama.common.audit.QBaseUserEntity _super = new com.ojosama.common.audit.QBaseUserEntity(this);
+
+    public final ComparablePath<java.util.UUID> categoryId = createComparable("categoryId", java.util.UUID.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final ComparablePath<java.util.UUID> createdBy = _super.createdBy;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> deletedAt = _super.deletedAt;
+
+    //inherited
+    public final ComparablePath<java.util.UUID> deletedBy = _super.deletedBy;
+
+    public final QEventInfo eventInfo;
+
+    public final ComparablePath<java.util.UUID> id = createComparable("id", java.util.UUID.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    //inherited
+    public final ComparablePath<java.util.UUID> updatedBy = _super.updatedBy;
+
+    public final ComparablePath<java.util.UUID> userId = createComparable("userId", java.util.UUID.class);
+
+    public QFavorite(String variable) {
+        this(Favorite.class, forVariable(variable), INITS);
+    }
+
+    public QFavorite(Path<? extends Favorite> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QFavorite(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QFavorite(PathMetadata metadata, PathInits inits) {
+        this(Favorite.class, metadata, inits);
+    }
+
+    public QFavorite(Class<? extends Favorite> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.eventInfo = inits.isInitialized("eventInfo") ? new QEventInfo(forProperty("eventInfo")) : null;
+    }
+
+}
+
