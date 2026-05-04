bac4a576e540c882ef47f3466bcc4d6de29d76f5
YoungJae1118 <apricity1118@gmail.com>
2026-05-04 11:18:02 +0900

    refactor: UserServiceApplication 수정 Evente consmer profile 설정

--- user-service/src/main/java/com/ojosama/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/ojosama/userservice/UserServiceApplication.java
@@ -4,10 +4,22 @@ import com.ojosama.userservice.global.security.InternalApiProperties;
 import com.ojosama.userservice.global.security.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableConfigurationProperties({JwtProperties.class, InternalApiProperties.class})
+@ComponentScan(basePackages = "com.ojosama")
+@EntityScan(basePackages = {
+        "com.ojosama.userservice.domain.model",
+        "com.ojosama.common.kafka.domain"
+})
+@EnableJpaRepositories(basePackages = {
+        "com.ojosama.userservice.domain.repository",
+        "com.ojosama.common.kafka.domain"
+})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
--- user-service/src/main/java/com/ojosama/userservice/infrastructure/messaging/kafka/consumer/UserBlacklistStatusEventConsumer.java
+++ user-service/src/main/java/com/ojosama/userservice/infrastructure/messaging/kafka/consumer/UserBlacklistStatusEventConsumer.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@Profile("!local & !test")
+@Profile("!test")
 @RequiredArgsConstructor
 public class UserBlacklistStatusEventConsumer {
 
