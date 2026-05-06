9a22ed7058b3fe392c20aac7ff5b7724337f2fe4
gsemily <gsemily22@gmail.com>
2026-05-06 16:00:32 +0900

    refactor: 입력값 변경

--- operation-service/src/main/java/com/ojosama/report/presentation/dto/UpdateReportRequest.java
+++ operation-service/src/main/java/com/ojosama/report/presentation/dto/UpdateReportRequest.java
@@ -1,17 +1,18 @@
 package com.ojosama.report.presentation.dto;
 
 import com.ojosama.report.application.dto.command.UpdateReportCommand;
+import com.ojosama.report.domain.model.enums.ReportStatus;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateReportRequest(
         @NotNull(message = "제재 확정 여부를 선택해주세요.")
-        Boolean isResolved,
+        ReportStatus status,
 
         @NotBlank(message = "관리자 처리 사유를 입력해주세요.")
         String operatorMemo
 ) {
     public UpdateReportCommand toCommand() {
-        return new UpdateReportCommand(isResolved, operatorMemo);
+        return new UpdateReportCommand(status, operatorMemo);
     }
 }
