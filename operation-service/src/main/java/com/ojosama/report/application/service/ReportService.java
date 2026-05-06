21f58574945ca36d17b781a8f68baa3fea8a5408
gsemily <gsemily22@gmail.com>
2026-05-06 14:46:59 +0900

    refactor: 신고 및 블라인드 카운트 판단 연산자 변경

--- operation-service/src/main/java/com/ojosama/report/application/service/ReportService.java
+++ operation-service/src/main/java/com/ojosama/report/application/service/ReportService.java
@@ -114,7 +114,7 @@ public class ReportService {
         long reportCount = reportRepository.countByTargetId(command.targetId());
 
         // 신고가 3회 이상이면 신고 대상 블라인드 처리 및 유저 블랙리스트 조건 검사
-        if (reportCount + 1 == 3) {
+        if (reportCount >= 3) {
             publishBlindEvent(command);
             publishBlacklistReviewRequestEvent(command.targetUserId());
         }
@@ -144,13 +144,12 @@ public class ReportService {
 
     private void publishBlacklistReviewRequestEvent(UUID targetUserId) {
         long userBlindCount = reportRepository.countBlindedTargetByUserId(targetUserId);
-        long blindCountAfter = userBlindCount + 1;
 
-        if (blindCountAfter >= 5) {
+        if (userBlindCount >= 5) {
             BlacklistReviewRequestEvent event = new BlacklistReviewRequestEvent(
                     targetUserId,
                     "블라인드 처리가 5회 누적되어 블랙리스트 등록 검토가 필요합니다.",
-                    blindCountAfter
+                    userBlindCount
             );
 
             outbox.publish(
