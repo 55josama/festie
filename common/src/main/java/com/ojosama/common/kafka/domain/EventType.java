679212c858dc5b793e96b8ed29a2f47602859601
Lee Subin <99sbin@gmail.com>
2026-05-06 21:43:45 +0900

    feat : Event Consumer, Dto 작성 (EventType 추가)

--- common/src/main/java/com/ojosama/common/kafka/domain/EventType.java
+++ common/src/main/java/com/ojosama/common/kafka/domain/EventType.java
@@ -18,7 +18,7 @@ public enum EventType {
     COMMENT_UPDATED("CommentUpdated"),
     POST_REPORTED("PostReported"),
     COMMENT_REPORTED("CommentReported"),
-    
+
     // event
     EVENT_CREATED("EventCreated"),
     EVENT_DELETED("EventDeleted"),
@@ -36,6 +36,7 @@ public enum EventType {
 
     // operation
     REPORT_BLINDED("ReportBlinded"),
+    REPORT_UNBLINDED("ReportUnblinded"),
     BLACKLIST_REGISTERED("BlacklistRegistered"),
     BLACKLIST_UPDATED("BlacklistUpdated"),
     BLACKLIST_REVIEW_REQUESTED("BlacklistReviewRequested"),
