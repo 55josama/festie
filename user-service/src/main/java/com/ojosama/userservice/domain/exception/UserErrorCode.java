a2fe6ffab122d154aa182689d1d89bd62f27af47
YoungJae1118 <apricity1118@gmail.com>
2026-05-07 14:59:11 +0900

    refactor: Black List Null일 시 NPE 방지, 공통 예외 처리

--- user-service/src/main/java/com/ojosama/userservice/domain/exception/UserErrorCode.java
+++ user-service/src/main/java/com/ojosama/userservice/domain/exception/UserErrorCode.java
@@ -20,6 +20,7 @@ public enum UserErrorCode implements ErrorCode {
     REFRESH_TOKEN_HASH_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Refresh Token 해시 생성에 실패했습니다."),
 
 
+    INVALID_BLACKLIST_STATUS(HttpStatus.BAD_REQUEST, "알 수 없는 블랙리스트 상태입니다."),
     BLOCKED_USER(HttpStatus.FORBIDDEN, "차단된 사용자는 로그인할 수 없습니다."),
     INVALID_CATEGORY(HttpStatus.BAD_REQUEST, "존재하지 않는 카테고리입니다.");
 
