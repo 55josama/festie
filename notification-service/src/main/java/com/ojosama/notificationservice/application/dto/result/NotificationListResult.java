6bdd70183892fc3d16b0a81ab01d32895b78858d
rmkim4@gmail.com <rmkim4@gmail.com>
2026-05-08 03:23:25 +0900

    refactor: 제거 불필요한 DTO(NotificationListResult, NotificationResponse)

--- notification-service/src/main/java/com/ojosama/notificationservice/application/dto/result/NotificationListResult.java
+++
@@ -1,9 +0,0 @@
-package com.ojosama.notificationservice.application.dto.result;
-
-import java.util.List;
-
-public record NotificationListResult(
-        List<NotificationResult> notifications,
-        int count
-) {
-}
