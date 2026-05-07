5e643b1942adc278d25cbe7ed2a1a19ca5ba987e
rmkim4@gmail.com <rmkim4@gmail.com>
2026-05-08 03:26:27 +0900

    fix: 알림 자동 삭제 기준 15일로 수정

--- notification-service/src/main/java/com/ojosama/notificationservice/infrastructure/scheduler/NotificationScheduler.java
+++ notification-service/src/main/java/com/ojosama/notificationservice/infrastructure/scheduler/NotificationScheduler.java
@@ -18,8 +18,7 @@ public class NotificationScheduler {
     @Scheduled(cron = "0 0 3 * * *")
     @Transactional
     public void cleanNotifications() {
-        // LocalDateTime time = LocalDateTime.now().minusDays(15);
-        LocalDateTime time = LocalDateTime.now();
+        LocalDateTime time = LocalDateTime.now().minusDays(15);
         Long count = notificationRepository.deleteOldNotifications(time);
 
         log.info("자동 삭제 된 알림 {}건, 기준 날짜 {}", count, time);
