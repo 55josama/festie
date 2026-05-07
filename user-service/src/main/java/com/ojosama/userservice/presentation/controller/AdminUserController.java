ef586c2df44e0754d61ee9056318cb29a15b59d5
YoungJae1118 <apricity1118@gmail.com>
2026-05-07 12:26:23 +0900

    feat: AdminUserController 공통 응답 적용

--- user-service/src/main/java/com/ojosama/userservice/presentation/controller/AdminUserController.java
+++ user-service/src/main/java/com/ojosama/userservice/presentation/controller/AdminUserController.java
@@ -1,5 +1,6 @@
 package com.ojosama.userservice.presentation.controller;
 
+import com.ojosama.common.response.ApiResponse;
 import com.ojosama.userservice.application.dto.query.AdminDetailUserQuery;
 import com.ojosama.userservice.application.dto.result.AdminUserDetailResult;
 import com.ojosama.userservice.application.service.UserAdminService;
@@ -8,6 +9,7 @@ import com.ojosama.userservice.presentation.dto.request.AdminUserListRequestDto;
 import com.ojosama.userservice.presentation.dto.response.AdminChangeUserRoleResponseDto;
 import com.ojosama.userservice.presentation.dto.response.AdminDetailUserResponseDto;
 import com.ojosama.userservice.presentation.dto.response.AdminUserListResponseDto;
+import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -30,32 +32,36 @@ public class AdminUserController {
 
     // 회원 목록 조회
     @GetMapping
-    public Page<AdminUserListResponseDto> getUsers(
+    public ApiResponse<Page<AdminUserListResponseDto>> getUsers(
             @ModelAttribute AdminUserListRequestDto request
     ) {
-        return adminUserService.getUsers(request.toQuery())
+        Page<AdminUserListResponseDto> response = adminUserService.getUsers(request.toQuery())
                 .map(AdminUserListResponseDto::from);
+
+        return ApiResponse.success(response);
     }
 
     // 회원 상세 조회
     @GetMapping("/{userId}")
-    public AdminDetailUserResponseDto getDetailUser(
+    public ApiResponse<AdminDetailUserResponseDto> getDetailUser(
             @PathVariable UUID userId
     ) {
         AdminDetailUserQuery query = new AdminDetailUserQuery(userId);
         AdminUserDetailResult result = adminUserService.getDetailUser(query);
 
-        return AdminDetailUserResponseDto.from(result);
+        return ApiResponse.success(AdminDetailUserResponseDto.from(result));
     }
 
     // 유저 권한 수정
     @PatchMapping("/{userId}/role")
-    public AdminChangeUserRoleResponseDto changeUserRole(
+    public ApiResponse<AdminChangeUserRoleResponseDto> changeUserRole(
             @PathVariable UUID userId,
-            @RequestBody AdminChangeUserRoleRequestDto request
+            @Valid @RequestBody AdminChangeUserRoleRequestDto request
     ) {
-        return AdminChangeUserRoleResponseDto.from(
-                adminUserService.changeUserRole(request.toCommand(userId))
+        return ApiResponse.success(
+                AdminChangeUserRoleResponseDto.from(
+                        adminUserService.changeUserRole(request.toCommand(userId))
+                )
         );
     }
 }
