404cd4e9ea3b094fe82bd420fa9a78a37b24ba31
rmkim4@gmail.com <rmkim4@gmail.com>
2026-05-07 15:04:12 +0900

    fix: @Param 누락 추가

--- calendar-service/src/main/java/com/ojosama/calendarservice/calendar/infrastructure/persistence/JpaCalendarRepository.java
+++ calendar-service/src/main/java/com/ojosama/calendarservice/calendar/infrastructure/persistence/JpaCalendarRepository.java
@@ -27,7 +27,7 @@ public interface JpaCalendarRepository extends JpaRepository<Calendar, UUID>, Ca
                                                                                                 UUID userId);
 
     @Query("select c from Calendar c where c.eventInfo.eventDate >= :start AND c.eventInfo.eventDate < :end AND c.deletedAt is null AND c.eventStatus = true")
-    List<Calendar> findByEventInfo_EventDateAndDeletedAtIsNullAndEventStatusTrue(LocalDateTime start,
-                                                                                 LocalDateTime end);
+    List<Calendar> findByEventInfo_EventDateAndDeletedAtIsNullAndEventStatusTrue(@Param("start") LocalDateTime start,
+                                                                                 @Param("end") LocalDateTime end);
 
 }
