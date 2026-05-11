ffca1e65d0552b8c548a7c6348eb11f43147c341
KimRanmi <rmkim4@gmail.com>
2026-05-11 12:18:49 +0900

    [REFACTOR] 찜 서비스 리팩토링, 행사 상태 카프카 구독 (#207)
    * fix: 코드래빗 적용 브랜치 추가
    
    * refactor: 브랜치 이름 수정
    
    * remove: 필요없는 파일 삭제
    
    * refactor: 이벤트 삭제 로직 처리 방식 개선
    
    * feat: 카프카 컨슈머 추가
    
    * feat: 에러코드 추가
    
    * refactor: dto 추가, EventUpdatedConsumer의 dispatch 로직을 별도 메서드로 분리,
    
    * feat: 이벤트 상태 업데이트 처리 추가 및 이벤트 관련 메서드 리팩토링
    
    * fix: 수정된 에러코드 메시지 업데이트
    
    * feat: EventStatus enum 확장 및 Kafka 컨슈머 로직 리팩토링
    
    * refactor: EventInfo에서 불필요한 필드 제거
    
    * refactor: FavoriteService 메서드 리팩토링 및 FavoriteRepository 개선
    
    * refactor: dispatch 메서드 예외 처리 추가 및 Kafka 이벤트 로깅 메시지 수정
    
    * fix: 코드래빗 수정
    
    ---------
    
    Co-authored-by: gsemily <gsemily22@gmail.com>

--- favorite-service/src/main/java/com/ojosama/favoriteservice/infrastructure/persistence/FavoriteRepositoryCustom.java
+++ favorite-service/src/main/java/com/ojosama/favoriteservice/infrastructure/persistence/FavoriteRepositoryCustom.java
@@ -1,8 +1,15 @@
 package com.ojosama.favoriteservice.infrastructure.persistence;
 
+import com.ojosama.favoriteservice.domain.model.EventFieldChange;
+import com.ojosama.favoriteservice.domain.model.EventStatus;
+import java.util.List;
 import java.util.UUID;
 
 public interface FavoriteRepositoryCustom {
 
-    void updateEventInfoBulk(UUID eventId, String field, String after);
+    void updateEventInfoBulk(UUID eventId, List<EventFieldChange> changedFields);
+
+    void deleteAllByEventId(UUID eventId);
+
+    void updateStatusAllByEventId(UUID eventId, EventStatus status);
 }
