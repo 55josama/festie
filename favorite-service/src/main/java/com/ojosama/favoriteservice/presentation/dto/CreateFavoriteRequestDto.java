e93eb9051c8f1acda8a2d87ccc73cc4aba5e479b
rmkim4@gmail.com <rmkim4@gmail.com>
2026-05-13 14:00:17 +0900

    refactor: @Builder 삭제

--- favorite-service/src/main/java/com/ojosama/favoriteservice/presentation/dto/CreateFavoriteRequestDto.java
+++ favorite-service/src/main/java/com/ojosama/favoriteservice/presentation/dto/CreateFavoriteRequestDto.java
@@ -3,9 +3,7 @@ package com.ojosama.favoriteservice.presentation.dto;
 import com.ojosama.favoriteservice.application.dto.command.CreateFavoriteCommand;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
-import lombok.Builder;
 
-@Builder
 public record CreateFavoriteRequestDto(
 
         @NotNull(message = "이벤트ID는 필수입니다.")
@@ -13,7 +11,7 @@ public record CreateFavoriteRequestDto(
 
         @NotNull(message = "카테고리ID는 필수입니다.")
         UUID categoryId
-        
+
 ) {
     public CreateFavoriteCommand toCommand() {
         return new CreateFavoriteCommand(eventId(), categoryId());
