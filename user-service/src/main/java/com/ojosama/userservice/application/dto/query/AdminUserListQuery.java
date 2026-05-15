7bc74947f36d8bcf20ef5be1a2566eeb1d17e438
Lee Subin <99sbin@gmail.com>
2026-05-16 01:02:04 +0900

    feat : 관리자의 유저 조회 ,권한 부여,페이징 기능 추가

--- user-service/src/main/java/com/ojosama/userservice/application/dto/query/AdminUserListQuery.java
+++ user-service/src/main/java/com/ojosama/userservice/application/dto/query/AdminUserListQuery.java
@@ -1,6 +1,11 @@
 package com.ojosama.userservice.application.dto.query;
 
+import com.ojosama.userservice.domain.model.UserRole;
+
 public record AdminUserListQuery(
+        String email,
+        String name,
+        UserRole role,
         int page,
         int size
 ) {
