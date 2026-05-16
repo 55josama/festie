20981cfe324da3ec4c4057337e6366c1f61d36d0
subin <143770193+LSLight@users.noreply.github.com>
2026-05-16 18:04:49 +0900

    Update OperationRequestController.java

--- operation-service/src/main/java/com/ojosama/operationrequest/presentation/controller/OperationRequestController.java
+++ operation-service/src/main/java/com/ojosama/operationrequest/presentation/controller/OperationRequestController.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
