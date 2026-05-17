cb1c433bc514445fda2ad79c9be62309a46fe408
rmkim4@gmail.com <rmkim4@gmail.com>
2026-05-18 01:42:41 +0900

    fix: auditor 수정

--- common/src/main/java/com/ojosama/common/audit/AuditorAwareImpl.java
+++ common/src/main/java/com/ojosama/common/audit/AuditorAwareImpl.java
@@ -2,14 +2,38 @@ package com.ojosama.common.audit;
 
 import java.util.Optional;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class AuditorAwareImpl implements AuditorAware<UUID> {
 
     @Override
     public Optional<UUID> getCurrentAuditor() {
-        return Optional.of(UUID.fromString("00000000-0000-0000-0000-000000000000")); //테스트를 위한 임의값 지정
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        log.info("### authentication: {}", authentication);
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+
+        Object principal = authentication.getPrincipal();
+
+        log.info("### principal: {}", principal);
+
+        if (principal instanceof UUID uuid) {
+            return Optional.of(uuid);
+        }
+
+        if (principal instanceof String str) {
+            return Optional.of(UUID.fromString(str));
+        }
+
+        return Optional.empty();
     }
 }
